61cbc0bfbcc758fa819dfdd6e0c70b7b4cce4af5
Siima Samson <siimasamson79@gmail.com>
2025-04-01 17:05:49 +0300

    footer padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,30 +3,24 @@
 import { motion } from "framer-motion";
 
 export default function Footer() {
+    const currentYear = new Date().getFullYear(); // Get current year
+
     return (
-        <footer style={{ textAlign: "center", padding: "1rem", background: "#f1f1f1", overflow: "hidden" }}>
+        <footer style={{ 
+            textAlign: "center", 
+            padding: "1.5rem", 
+            background: "#f1f1f1", 
+            overflow: "hidden", 
+            fontWeight: "500" 
+            
+        }}>
             <motion.p
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ duration: 1 }}
                 style={{ display: "inline-block" }}
             >
-                <motion.span
-                    initial={{ x: "-100%" }}
-                    animate={{ x: 0 }}
-                    transition={{ duration: 2, ease: "easeOut" }}
-                    style={{ display: "inline-block" }}
-                >
-                    Code With Siima.
-                </motion.span>{" "}
-                <motion.span
-                    initial={{ x: "100%" }}
-                    animate={{ x: 0 }}
-                    transition={{ duration: 2, ease: "easeOut" }}
-                    style={{ display: "inline-block" }}
-                >
-                    All rights reserved.
-                </motion.span>
+                Siima | Samson &copy; {currentYear}
             </motion.p>
         </footer>
     );
