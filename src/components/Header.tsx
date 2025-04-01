670deb9aa8546d7db603c4bffa0a621eb3664c24
Siima Samson <siimasamson79@gmail.com>
2025-04-01 16:36:49 +0300

    added my favicon and changed the title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { useState } from 'react'; // Import useState
 import styles from './Header.module.css'; // Importing CSS Module for styles
+import Head from 'next/head';
 
 const links = [
     { name: 'HOME', href: '/' },
@@ -28,49 +29,58 @@ const Header = () => {
     };
 
     return (
-        <header
-            style={{
-                position: 'absolute',
-                top: '0',
-                left: '0',
-                width: '100%',
-                padding: '20px',
-                display: 'flex',
-                zIndex: '10',
-                justifyContent: 'flex-end',
-            }}
-        >
-            <nav>
-                {/* Hamburger Menu for small screens */}
-                <div className={styles.hamburgerMenu} onClick={toggleMenu}> {/* Added onClick handler */}
-                    <button>☰</button> {/* Simple hamburger icon */}
-                </div>
+        <>
+            <Head>
+                <title>Siima | Portfolio</title>
+                <link rel="icon" href="/favicon.ico" type="image/x-icon" />
 
-                {/* Navigation Items */}
-                <ul className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ''}`}> {/* Conditionally add a class to toggle menu */}
-                    {links.map((link, index) => (
-                        <motion.li
-                            key={link.name}
-                            initial={{ y: -50, opacity: 0 }} // Start above
-                            animate={{ y: 0, opacity: 1 }} // Drop to position
-                            transition={{
-                                duration: 1,
-                                delay: index * 0.2 // Stagger animation from right to left
-                            }}
-                            className={styles.navItem} 
-                        >
-                            <Link 
-                                href={link.href} 
-                                className={styles.navLink} 
-                                onClick={handleLinkClick} // Close the menu when a link is clicked
+            </Head>
+
+
+            <header
+                style={{
+                    position: 'absolute',
+                    top: '0',
+                    left: '0',
+                    width: '100%',
+                    padding: '20px',
+                    display: 'flex',
+                    zIndex: '10',
+                    justifyContent: 'flex-end',
+                }}
+            >
+                <nav>
+                    {/* Hamburger Menu for small screens */}
+                    <div className={styles.hamburgerMenu} onClick={toggleMenu}> {/* Added onClick handler */}
+                        <button>☰</button> {/* Simple hamburger icon */}
+                    </div>
+
+                    {/* Navigation Items */}
+                    <ul className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ''}`}> {/* Conditionally add a class to toggle menu */}
+                        {links.map((link, index) => (
+                            <motion.li
+                                key={link.name}
+                                initial={{ y: -50, opacity: 0 }} // Start above
+                                animate={{ y: 0, opacity: 1 }} // Drop to position
+                                transition={{
+                                    duration: 1,
+                                    delay: index * 0.2 // Stagger animation from right to left
+                                }}
+                                className={styles.navItem}
                             >
-                                {link.name}
-                            </Link>
-                        </motion.li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
+                                <Link
+                                    href={link.href}
+                                    className={styles.navLink}
+                                    onClick={handleLinkClick} // Close the menu when a link is clicked
+                                >
+                                    {link.name}
+                                </Link>
+                            </motion.li>
+                        ))}
+                    </ul>
+                </nav>
+            </header>
+        </>
     );
 };
 
